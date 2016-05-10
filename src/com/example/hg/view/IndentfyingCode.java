b48bcee524139c3ff59722c595567481d5963822
ssc <356494994@qq.com>
2016-05-11 00:03:32 +0800

    m

--- src/com/example/hg/view/IndentfyingCode.java
+++ src/com/example/hg/view/IndentfyingCode.java
@@ -43,37 +43,50 @@ public class IndentfyingCode extends View {
 	private int lines=3;
 	/**验证码长度*/
 	private int textLength=4;
+	/**
+	 * 构造函数
+	 * @param context
+	 */
 	public IndentfyingCode(Context context) {
-		this(context, null);
-		// TODO Auto-generated constructor stub
-	
+		this(context, null);	
 	}
-
+	/**
+	 * 构造函数
+	 * @param context
+	 * @param attrs
+	 */
 	public IndentfyingCode(Context context, AttributeSet attrs) {
-		this(context, attrs, 0);
-		// TODO Auto-generated constructor stub
-		
+		this(context, attrs, 0);		
 	}
-
+	/**
+	 * 构造函数
+	 * @param context
+	 * @param attrs
+	 * @param defStyle
+	 */
 	public IndentfyingCode(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		// TODO Auto-generated constructor stub
 		this.context=context;
 		random = new Random();
-		TypedArray a = context.getTheme().obtainStyledAttributes(attrs,
-				R.styleable.IndentfyingCode, defStyle, 0);
+		TypedArray a = context.getTheme().obtainStyledAttributes(attrs,R.styleable.IndentfyingCode, defStyle, 0);
+		System.err.println("-----------------1-------------------a==>"+a.getIndexCount());
 		for (int i = 0; i < a.getIndexCount(); i++) {
-			switch (a.getIndex(i)) {
+			System.err.println("------------------a.getIndex(i)=>"+a.getIndex(i));
+			switch (a.getIndex(i)) {			
 			case R.styleable.IndentfyingCode_titleText:
+				System.err.println("------------------------------------02");
 //				text = a.getString(a.getIndex(i));
 				text = text(textLength);
 				LogShow.d(text);
 				break;
 
 			case R.styleable.IndentfyingCode_titleTextColor:
+				System.err.println("------------------------------------03");
 				textColor = a.getColor(a.getIndex(i), Color.BLACK);
 				break;
 			case R.styleable.IndentfyingCode_titleTextSize:
+				System.err.println("------------------------------------04");
 				textSize = a.getDimensionPixelSize(a.getIndex(i),
 						(int) TypedValue.applyDimension(
 								TypedValue.COMPLEX_UNIT_SP, 14, getResources()
@@ -81,22 +94,34 @@ public class IndentfyingCode extends View {
 				break;
 			}
 		}
-
+		System.err.println("------------------------------------2");
 		a.recycle();
+		System.err.println("-------------a.recycle()==>");
 		mPaint = new Paint();
+		System.err.println("-------------------textSize----------4------->"+textSize);
 		mPaint.setTextSize(textSize);
+		System.err.println("------------------------------------5");
 		mPaint.setColor(textColor);
+		System.err.println("------------------------textColor-==>6-->"+textColor);
 		rect = new Rect();
-		
+		System.err.println("------------------------------------7");
+		System.out.println("=========text================"+text);
+		System.out.println("===========textlength=============="+text.length());
+		System.out.println("=============rect============"+rect);
+		System.out.println("=========================");
 		mPaint.getTextBounds(text, 0, text.length(), rect);
+		System.err.println("------------------------------------8");
 		textCodeWid=rect.width();
+		System.err.println("------------------------------------9");
 		textCodeHei=rect.height();
+		System.err.println("------------------------------------10");
 		
 		this.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
+				System.err.println("------------------------------------11");
 				text = text(textLength);
 			
 				postInvalidate();
@@ -141,7 +166,9 @@ public class IndentfyingCode extends View {
 		}
 		return measureSize + pading1 + pading2;
 	}
-
+	/**
+	 * 绘制图案
+	 */
 	@Override
 	protected void onDraw(Canvas canvas) {
 		// TODO Auto-generated method stub
